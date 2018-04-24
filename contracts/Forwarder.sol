4a681d36fb537ea5df1b99d17e278d29658dd043
Marco Argentieri <marco@vulpem.com>
2018-04-24 13:53:03 +0000

    Merged marcoargentieri/forward-only-ethers-because-tokens-are-m-1524573001032 in master (pull request #2)
    forward only ethers, because tokens are manual triggered. No any utility actually. Better to think other way to accept token and credit to the user in centralized database (exchnages, icos) or neither accept at all :P
    
    Approved-by: altafan <pietro@vulpem.com>

--- contracts/Forwarder.sol
+++ contracts/Forwarder.sol
@@ -1,14 +1,7 @@
 pragma solidity ^0.4.21;
 
-contract ERC20Basic {
-    function totalSupply() public view returns (uint256);
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-}
-
 contract Forwarder {
-    // Address to which any funds (ETH or ERC20 token) sent to this contract will be forwarded
+    // Address to which any funds (ETH) sent to this contract will be forwarded
     address public beneficiary;
 
     /**
@@ -25,11 +18,4 @@ contract Forwarder {
         // throws on failure
         beneficiary.transfer(msg.value);
     }
-
-    function sendTokens(ERC20Basic token) public {
-        uint256 amount = token.balanceOf(this);
-
-        token.transfer(beneficiary, amount);
-    }
-
 }
