3b864d3e5714f90faa47bd249c0a1bc5ca10f870
Marco Argentieri <marco@vulpem.com>
2018-04-24 13:11:49 +0000

    Merged in marcoargentieri/forwarderfactory-minimal-gas-usage-1524572684352 (pull request #1)
    ForwarderFactory minimal gas usage && dynamic address input
    
    Approved-by: altafan <pietro@vulpem.com>

--- contracts/ForwarderFactory.sol
+++ contracts/ForwarderFactory.sol
@@ -3,19 +3,14 @@ pragma solidity ^0.4.21;
 import "./Forwarder.sol";
 
 contract ForwarderFactory {
-    address[] public contracts;
 
     event ContractDeployed(address forwarderAddress);
     
-    function create(uint256 number) public {
-        require(number <= 50);
-
+    function create(address beneficiary, uint256 number) public {
+    
         for (uint256 i = 0; i < number; i++) { 
-            address _contract = new Forwarder(msg.sender);
-
-            contracts.push(_contract);
-
-            emit ContractDeployed(_contract);
+            emit ContractDeployed(new Forwarder(beneficiary));
         }
+        
     }
 }
