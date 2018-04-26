e73f6c20b561b57ebf15ae9f85d82d066879a14c
altafan <pietralberto.mazza@gmail.com>
2018-04-26 17:27:05 +0200

    store decoded events from Factory create() in json file

--- test/TestForwarder.js
+++ test/TestForwarder.js
@@ -1,10 +1,8 @@
 const Forwarder = artifacts.require("Forwarder.sol")
-const Token = artifacts.require("MockToken.sol")
 
 contract("Forwarder", async (accounts) => {
 
   beforeEach(async () => {
-    this.token = await Token.new()
     this.forwarder = await Forwarder.new(accounts[0])
   })
 
@@ -18,25 +16,6 @@ contract("Forwarder", async (accounts) => {
 
     assert.equal(nextBalance - prevBalance, 2 * Math.pow(10, 18), "should be equal")
     assert.equal(contractBalance, 0, "should be equal to zero")
-  })
-
-  it("should forward erc20 token sent to the contrat to the owner", async () =>  {
-    const amount = 1000 * Math.pow(10, 8)
-
-    await this.token.transfer(this.forwarder.address, amount, { from: accounts[0] })
-
-    const prevBalance = await this.token.balanceOf(this.forwarder.address)
-    
-    assert.equal(prevBalance.toNumber(), amount, "should be equal")
-
-    await this.forwarder.sendTokens(this.token.address);
-
-    const nextBalance = await this.token.balanceOf(this.forwarder.address)
-    const ownerBalance = await this.token.balanceOf(accounts[0])
-    const totalSupply = await this.token.totalSupply()
-
-    assert.equal(nextBalance.toNumber(), 0, "should be equal to zero")
-    assert.equal(ownerBalance.toNumber(), totalSupply.toNumber(), "should be equal")
   })
 
 })
